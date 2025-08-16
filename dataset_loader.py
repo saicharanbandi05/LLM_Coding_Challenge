97f8deea41e6d7e3c9ee3c7ce644651107842096
admin <admin@admins-MacBook-Air.local>
2025-08-16 16:05:00 +0530

    dataset calling

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,8 +1,5 @@
 from datasets import load_dataset
 
 def load_fsdd_dataset():
-    """
-    Loads the Free Spoken Digit Dataset (FSDD) from Hugging Face.
-    """
     print("Loading dataset...")
     return load_dataset("mteb/free-spoken-digit-dataset", split="train")
